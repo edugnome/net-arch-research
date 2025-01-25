4824001581b7c31d5ba84ce796fd8d3951116c1d
Maxim Bolshim <maxim.bolshim@yandex.ru>
2025-01-25 23:10:48 +0300

    Рефакторинг

---
+++ analisys/gradients.py
@@ -0,0 +1,36 @@
+def extract_gradients_by_layer(model):
+    """
+    Извлекает и выводит градиенты для каждого слоя модели PyTorch.
+
+    Эта функция перебирает все именованные параметры модели, собирает их
+    градиенты (если они существуют) и выводит их послойно.
+
+    Параметры:
+    ----------
+    model : torch.nn.Module
+        Модель PyTorch, из которой извлекаются градиенты. Для наличия градиентов
+        модель должна пройти обратное распространение ошибки.
+
+    Возвращает:
+    -----------
+    dict
+        Словарь, сопоставляющий имена слоев с их соответствующими градиентами.
+        Если у слоя нет градиента, его значение будет None.
+
+    Пример:
+    -------
+    >>> model = torch.nn.Linear(10, 5)
+    >>> loss = criterion(model(x), y)
+    >>> loss.backward()
+    >>> grads = extract_gradients_by_layer(model)
+    """
+
+    grads = {}
+    for name, param in model.named_parameters():
+        if param.grad is not None:
+           grads[name] = param.grad.detach().clone()
+        else:
+            grads[name] = None
+    print("Градиенты по слоям:")
+    for layer_name, grad in grads.items():
+        print(f"{layer_name}: {grad}")
